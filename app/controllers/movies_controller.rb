787ffa4995f6d428fb4657ad25529bde3c1720b1
Rajavi Mishra <rajavi.mishra@berkeley.edu>
2020-10-23 07:01:33 +0000

    checkinggg

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,6 @@ class MoviesController < ApplicationController
     @ratings_to_show = []  
     if (!params[:ratings].nil?)
       @ratings_to_show = params[:ratings].keys
-    else
-      @ratings_to_show = @all_ratings
-    end
     @movies = Movie.with_ratings(@ratings_to_show)
   end
 
